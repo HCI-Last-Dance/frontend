308a013acbe49ebfecf0d79a49cb68caf688df78
seank021 <seahnnnnnn@gmail.com>
2025-05-18 01:56:26 +0900

    feat: initial settings

---
+++ src/pages/app.tsx
@@ -0,0 +1,8 @@
+export default function App() {
+    return (
+        <div className='flex flex-col items-center justify-center h-screen bg-gray-100'>
+            <p className='text-4xl font-bold mb-4'>HCI</p>
+            <p className='text-lg text-gray-700'>Vite + React + Tailwind CSS 설정 완료</p>
+        </div>
+    )
+}
