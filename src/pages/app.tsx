82a57e44d9bdb1d8c5b8fa6064731ad6fca16518
seank021 <seahnnnnnn@gmail.com>
2025-05-26 21:37:20 +0900

    fix: header position

--- src/pages/app.tsx
+++ src/pages/app.tsx
@@ -1,27 +1,35 @@
 import React from 'react'
-import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Routes, Navigate, useLocation } from 'react-router-dom'
 import Header from '../components/header'
 import Home from './home'
 import Detail from './detail'
 import Login from './login'
 
-const App: React.FC = () => {
+const AppRoutes: React.FC = () => {
     const isUser: boolean = localStorage.getItem('isUser') === 'true'
+    const location = useLocation()
+    const hideHeader: boolean = location.pathname === '/login'
 
     return (
-        <div>
-            <Header />
-            <Router>
-                <Routes>
-                    <Route path='/' element={isUser ? <Home /> : <Navigate to='/login' />} />
-                    <Route path='/login' element={!isUser ? <Login /> : <Navigate to='/' />} />
-                    <Route
-                        path='/detail/:videoId'
-                        element={isUser ? <Detail /> : <Navigate to='/login' />}
-                    />
-                </Routes>
-            </Router>
-        </div>
+        <>
+            {!hideHeader && <Header />}
+            <Routes>
+                <Route path='/' element={isUser ? <Home /> : <Navigate to='/login' />} />
+                <Route path='/login' element={!isUser ? <Login /> : <Navigate to='/' />} />
+                <Route
+                    path='/detail/:videoId'
+                    element={isUser ? <Detail /> : <Navigate to='/login' />}
+                />
+            </Routes>
+        </>
+    )
+}
+
+const App: React.FC = () => {
+    return (
+        <Router>
+            <AppRoutes />
+        </Router>
     )
 }
 
