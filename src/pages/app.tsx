f62664ab85efd10e88fc0fae7bef809d442b8f01
seank021 <seahnnnnnn@gmail.com>
2025-05-26 21:23:14 +0900

    feat: login

--- src/pages/app.tsx
+++ src/pages/app.tsx
@@ -2,12 +2,14 @@ import React from 'react'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Home from './home'
 import Detail from './detail'
+import Login from './login'
 
 const App: React.FC = () => {
     return (
         <Router>
             <Routes>
                 <Route path='/' element={<Home />} />
+                <Route path='/login' element={<Login />} />
                 <Route path='/detail/:videoId' element={<Detail />} />
             </Routes>
         </Router>
